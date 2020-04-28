5242fdf4a982106d87317439b6a51b67e733b5db
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-04-28 12:14:00 +1200

    Create RankedGradeBook Class

---
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook.GradeBooks
+{
+    public class RankedGradeBook : BaseGradeBook
+    {
+        private readonly string _name;
+
+        public RankedGradeBook(string name) : base(name)
+        {
+            _name = name;
+        }
+    }
+}
