f96fcfc66d70f5461fe11926dba904b9d666a240
Nisida Azzalini <nisida.azzalini@gmail.com>
2020-04-28 12:11:57 +1200

    Update StandardGradeBook Type

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook : BaseGradeBook
+    {
+        private readonly string _name;
+
+        public StandardGradeBook(string name) : base(name)
+        {
+            _name = name;
+            Type = Enums.GradeBookType.Standard;
+        }
+
+
+    }
+}
